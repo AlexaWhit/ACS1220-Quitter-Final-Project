94d403cd9a3e3bd80aaa2cc34720f9e5dd9d2f6d
alexa-whitney <alexawhitney@gmail.com>
2023-02-22 15:38:00 -0700

    updated requirements.txt to try render

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from quitter_app.extensions import *
 from quitter_app.main.routes import main
 from quitter_app.auth.routes import auth
-from datetime import datetime
 
 # app.register_blueprint(main)
 # app.register_blueprint(auth)
